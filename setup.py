24212ad85074f63650ea7b6daa66d852213a69f7
Nikhesh <nikhesh.p@kambala.co.in>
2024-07-29 14:48:48 +0530

    constant version removed

--- setup.py
+++ setup.py
@@ -8,23 +8,23 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="NorenRestApi",
-    version="0.0.29",
+    version="0.0.30",
     author="KumarAnand",
     author_email="kumar.anand@kambala.co.in",
     description="A package for NorenOMS",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
-    ),
+    ],
  install_requires=[
-    "requests==2.28.2",
-    "websocket-client==1.5.1",
-    "pandas==1.5.3",
-    "PyYAML==6.0.1",
+    "requests",
+    "websocket-client",
+    "pandas",
+    "PyYAML",
     ],
 )
 
